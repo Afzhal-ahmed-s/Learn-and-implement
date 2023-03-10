c87d7aa3b08fcff95c9f540c117f6e71608ea317
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-03-10 12:54:22 +0530

    Bitwise implementation

---
+++ MyLab/src/com/BitwiseOperator/Main.java
@@ -0,0 +1,23 @@
+package com.BitwiseOperator;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(1^2^1^2^4^7^3^7^3^4^10);
+
+		//Very useful for questions such as
+//Given a non-empty array of integers nums, every element appears twice except for one. 
+//Find that single one.
+//You must implement a solution with a linear runtime complexity and use only constant extra space.
+		
+		//1^2^3^4:
+		//0001
+		//0010
+		//0011
+		//0100
+		//ans 4
+		
+	}
+
+}
